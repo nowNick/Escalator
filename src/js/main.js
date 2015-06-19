8c71e58ea8c40ce43df6821ebbb8bbd00e3bb157
nowNick <mikolaj.nowak93@gmail.com>
2015-06-19 10:51:53 +0200

    Added steps

--- src/js/main.js
+++ src/js/main.js
@@ -6,23 +6,12 @@ var container, stats;
 var controls;
 var camera, scene, renderer;
 
-var group;
-
-var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
-
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 var prismSet;
-var yellowCube;
 var waypoints;
-var waypointId;
 var clock = new THREE.Clock();
-var leftPivot;
-var rightPivot;
+var run = false;
 
 var PrismCubeFactory = {
     getPrism : function (a) {
@@ -62,13 +51,42 @@ function StairsTape(length, height, prismAproxSize, parent) {
     var prismSize = prismAproxSize - (length % prismAproxSize); // to cover exactly
     var n = (length / prismSize)*3-3;
     var prismSet = [];
+
     for(var i=0; i<n; i++) {
+        var texture = THREE.ImageUtils.loadTexture( 'images/textures/step.jpg' );
+        var textureMaterial =  new THREE.MeshLambertMaterial({map:texture});
+        texture.anisotropy = renderer.getMaxAnisotropy();
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+
+
         var myPrism = new THREE.Mesh(
             PrismCubeFactory.getPrism(prismSize),
-            new THREE.MeshLambertMaterial( { color: 0x00FF00 } )
+            new THREE.MeshFaceMaterial(
+                [
+                    textureMaterial, // +x
+                    textureMaterial, // -x
+                    textureMaterial, // +y
+                    textureMaterial, // -y
+                    textureMaterial, // +z
+                    textureMaterial // -z
+                ])
         );
+
+        myPrism.geometry.computeBoundingBox();
+
+        var max = myPrism.geometry.boundingBox.max;
+        var min = myPrism.geometry.boundingBox.min;
+        var texHeight = max.y - min.y;
+        var width = max.x - min.x;
+
+        texture.repeat.set(width / 512 , texHeight / 512);
+
+        texture.needsUpdate = true;
+
         myPrism.position.set(parent.position.x +(i*prismSize)-10,41,10);
         myPrism.waypointId = 0;
+        myPrism.pivot = undefined;
         prismSet.push(myPrism);
         parent.add( myPrism );
     }
@@ -80,11 +98,8 @@ init();
 animate();
 
 function init() {
-
     container = document.createElement( 'div' );
     document.body.appendChild( container );
-
-
 
     scene = new THREE.Scene();
 
@@ -95,8 +110,20 @@ function init() {
     var light = new THREE.PointLight( 0xffffff, 0.8 );
     camera.add( light );
 
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setClearColor( 0xf0f0f0 );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
 
+    controls = new THREE.TrackballControls( camera, renderer.domElement );
+    controls.minDistance = 10;
+    controls.maxDistance = 500;
 
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    container.appendChild( stats.domElement );
 
 
     waypoints = [];
@@ -112,24 +139,17 @@ function init() {
     var higherH = 80;
 
 
-
     waypoints.push(new THREE.Vector3(leftCorner, lowH, depth));
     waypoints.push(new THREE.Vector3(stepStart, lowH, depth));
     waypoints.push(new THREE.Vector3(stepEnd, higherH, depth));
 
     waypoints.push(new THREE.Vector3(rightCorner, higherH, depth));
-    rightPivot = new THREE.Object3D();
-    rightPivot.position.set(rightCorner, (higherH-highH/2)+highH, depth);
-    rightPivot.waypointId = 3;
 
     waypoints.push(new THREE.Vector3(rightCorner, highH, depth));
 
     waypoints.push(new THREE.Vector3(stepEnd, highH, depth));
     waypoints.push(new THREE.Vector3(stepStart, lowerH, depth));
     waypoints.push(new THREE.Vector3(leftCorner, lowerH, depth));
-    leftPivot = new THREE.Object3D();
-    leftPivot.position.set(leftCorner, (lowH-lowerH/2)+lowerH, depth);
-    leftPivot.waypointId = 7;
 
     var prismSize = 10;
     prismSet = StairsTape(rightCorner - leftCorner, highH - lowH, prismSize, scene);
@@ -144,28 +164,17 @@ function init() {
         scene.add( cube );
     });
 
-
-
-    //
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setClearColor( 0xf0f0f0 );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
-
-    controls = new THREE.TrackballControls( camera, renderer.domElement );
-    controls.minDistance = 200;
-    controls.maxDistance = 500;
-
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    container.appendChild( stats.domElement );
-
-    //
     window.addEventListener( 'resize', onWindowResize, false );
+    document.addEventListener("keydown", onDocumentKeyDown, false);
 
+
+}
+
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if(keyCode == 32) { //space
+        run = (run === false);
+    }
 }
 
 function onWindowResize() {
@@ -186,31 +195,18 @@ function animate() {
     controls.update();
     var closeEnough = 1;
 
-    if (prismSet) {
+    if (prismSet && run) {
         prismSet.forEach(function (prism) {
             var dx = new THREE.Vector3();
             dx.subVectors(waypoints[prism.waypointId], prism.position);
             var move = dx.length() >= closeEnough;
 
-            //if(prism.waypointId === rightPivot.waypointId) {
-            //    rightPivot.add(prism);
-            //}
-            //else if (prism.waypointId === leftPivot.waypointId) {
-            //    leftPivot.add(prism);
-            //}
-            //else {
-            //    rightPivot.remove(prism);
-            //    leftPivot.remove(prism);
-                if(move) {
-                    dx.normalize();
-                    dx.multiplyScalar(dt*30.0);
-                    prism.position.add(dx);
-                }
-            //}
-
-            //rightPivot.rotation.z += 0.01 * dt;
-            //leftPivot.rotation.z += 0.01 * dt;
-            if(!move) {
+            if(move) {
+                dx.normalize();
+                dx.multiplyScalar(dt*30.0);
+                prism.position.add(dx);
+            }
+            else {
                 prism.waypointId = (prism.waypointId+1) % (waypoints.length);
             }
         })
